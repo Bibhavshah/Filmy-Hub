0c8f8edba8e7ebd9a20cf2cc76d545f7f4992775
Bibhavshah <202051212@iiitvadodara.ac.in>
2023-10-31 15:12:46 +0530

    Fixed the image issue

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,8 +1,7 @@
-import React from "react";
-import "./SingleContent.css";
-import img from "../../components/assets/Naruto.jpg";
-//import { img_300, unavailable } from "../../config/config";
-import Badge from "@mui/material/Badge";
+import React from 'react';
+import './SingleContent.css';
+import { img_300, unavailable } from '../../config/config';
+import Badge from '@mui/material/Badge';
 
 const SingleContent = ({
   id,
@@ -12,21 +11,23 @@ const SingleContent = ({
   media_type,
   vote_average,
 }) => {
+  const posterUrl = `${img_300}${poster}`;
+  console.log(posterUrl);
   return (
     <div className="media">
-      {/* <img
+      <img
         className="psoter"
-        src={poster ? `${img_300} | ${poster}}` : unavailable}
+        src={poster ? posterUrl : unavailable}
         alt={title}
-      /> */}
+      />
       <Badge
         badgeContent={Math.round(vote_average * 10) / 10}
-        color={vote_average > 6 ? "primary" : "secondary"}
+        color={vote_average > 6 ? 'primary' : 'secondary'}
       />
-      <img src={img} alt={title} />
+      {/* <img src={img} alt={title} /> */}
       <b className="title">{title}</b>
       <span className="sub-title">
-        {media_type === "tv" ? "TV Series" : "Movie"}
+        {media_type === 'tv' ? 'TV Series' : 'Movie'}
         <span className="sub-title">{date}</span>
       </span>
     </div>
